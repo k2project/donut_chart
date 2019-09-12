c1b43b049014faa60f602d458db31a9762514d43
k2project <kopczkrzy@gmail.com>
2019-09-12 23:08:56 +0100

    Updates

---
+++ precache-manifest.bf6076e4587533553c6281ff6059244a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2cc2b80b526ecb8228c142996f7b29",
+    "url": "/donut_chart/index.html"
+  },
+  {
+    "revision": "4054cbc1fabcb6f1e644",
+    "url": "/donut_chart/static/css/main.d0cdfa2d.chunk.css"
+  },
+  {
+    "revision": "686f141b77667f9f62e3",
+    "url": "/donut_chart/static/js/2.e278d863.chunk.js"
+  },
+  {
+    "revision": "4054cbc1fabcb6f1e644",
+    "url": "/donut_chart/static/js/main.ad1e2f95.chunk.js"
+  },
+  {
+    "revision": "cf06e616589a8ebdb780",
+    "url": "/donut_chart/static/js/runtime~main.2b531981.js"
+  }
+]);
